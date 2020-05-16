4f4392ced689b0c0e1ca49f4f803087649c030b9
unknown <webbdesmondj@gmail.com>
2020-05-16 12:40:36 -0700

    CSS Coding Started

--- datasciencedjw.py
+++ datasciencedjw.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Yo Yo Hello'
+    return render_template('index.html')
 
-@app.route('/Projects')
-def projects():
-    return 'Recent Works'
+@app.route('/resume')
+def Resume():
+    return render_template('resume.html')
+
+@app.route('/projects')
+def Projects():
+    return render_template('projects.html')
 
 if __name__ == "__main__":
     app.run()
