a00d04a54f4872e7e8ef7a881dd67f06ec594646
unknown <webbdesmondj@gmail.com>
2020-05-18 13:52:19 -0700

    More Template Work

--- datasciencedjw.py
+++ datasciencedjw.py
@@ -1,17 +1,23 @@
 from flask import Flask, redirect, url_for, render_template
+
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/resume')
 def Resume():
     return render_template('resume.html')
+
 
 @app.route('/projects')
 def Projects():
     return render_template('projects.html')
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
