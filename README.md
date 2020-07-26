a74e84c7b7a43e80b4200b3a4c442ea49098b4c6
unknown <webbdesmondj@gmail.com>
2020-07-26 16:25:35 -0700

    Minor README Update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# datasciencedjw
+# Datasciencedjw.com
+This is my personal built from scratch website meant to dispaly my data science and front end development portfolios. 
+
+#### Built Using:
+HTML
+Bootstrap for CSS
+Netlify
--- datasciencedjw.py
+++ datasciencedjw.py
@@ -2,22 +2,20 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-
-
-@app.route('/')
+@app.route("/")
 def home():
     return render_template('index.html')
 
 
 @app.route('/resume')
-def Resume():
+def resume():
     return render_template('resume.html')
 
 
 @app.route('/projects')
-def Projects():
+def projects():
     return render_template('projects.html')
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
